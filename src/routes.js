7ca83c6f88c31373010fa97120d323e6fb501925
Guigite <guicunhaalves@gmail.com>
2023-02-06 16:21:02 -0300

    commit 03

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = express.Router();
 const db = require('./db');
 
-routes.get("receitas", (req, res) => {
+routes.get("/receitas", (req, res) => {
     let query = "SELECT * FROM receitas";
     db.query(query, (err,result) => {
         if (err){
