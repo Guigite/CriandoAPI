697171c2abcff4c4b7aee0d2d672d579a65cb48c
Guigite <guicunhaalves@gmail.com>
2023-02-13 10:48:55 -0300

    Otimizando o server.js

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 const express = require('express');
 const server = express();
-const routes = require("./src/routes");
+// const routes = require("./src/routes");
+const db = require('./src/db');
 
-server.use(express.json());
+// server.use(express.json());
 
-server.use("/", routes);
+// server.use("/", routes);
+server.get("/", (req, res) =>{
+    
+    res.send("Bem vindo a API da Equipe Rocket!")
+})
+// server.get("/receitas", (req, res)=>{
+//     let query = "SELECT * FROM receitas"
+
+//     db.query(query, (err, result)=>{
+//         if(err){
+//             return console.log("Erro na consulta: "+ err)
+//         }
+
+//         return res.status(200).send(result)
+//     })
+// })
+server.get(["/receitas","/receitas/:tipo"], (req,res) =>{
+    if(req.params.tipo){
+        console.log(req.params)
+    let query = `SELECT * FROM receitas WHERE tipo="${req.params.tipo}"`
+    db.query(query, (err, result)=>{
+        if(err){
+            return console.log("Erro na consulta: "+ err)
+        }
+
+        return res.status(200).send(result)
+    })
+    }else{
+        let query = "SELECT * FROM receitas"
+
+        db.query(query, (err, result)=>{
+            if(err){
+                return console.log("Erro na consulta: "+ err) 
+            }
+    
+            return res.status(200).send(result)
+        })
+    }
+    
+})
+
 
 server.listen(8080, () => {
  console.log("ta funcionando");
